8abe43fa9966ca6aff28d368b0124f52eeb31ef8
John Dave Decano <johndavedecano@gmail.com>
2022-07-06 23:05:56 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "zones" {
+  type    = list(string)
+  default = ["a", "b", "c"]
+}
